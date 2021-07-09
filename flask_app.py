8c8c9de11adf17e7034b3d4bef9be145db30c1cd
Ashish <ashishkumaraswamy@gmail.com>
2021-07-09 17:43:37 +0530

    login and register page added db not linked

---
+++ flask_app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template('login.html', title="Login")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html", title="Register")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
